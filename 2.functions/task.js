b19840fd78c3344c7a08b8e988a04b9132ea5c56
Mikhail Ignatov <ignatov.m55@gmail.com>
2024-01-10 14:42:04 +0600

    Готово второе задание

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,8 @@
 function getArrayParams(...arr) {
-	let min = Infinity;
-	let max = -Infinity;
+	let min = arr[0];
+	let max = arr[0];
 	let sum = 0;
-	for (let i = 0; arr.length; i++) {
+	for (let i = 0; i < arr.length; i++) {
 		if (arr[i] > max) {
 			max = arr[i];
 		}
